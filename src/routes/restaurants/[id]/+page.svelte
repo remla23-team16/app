1758c77767e47b63b8b42f306151d889fbb0f1c0
Tim van Dam <timvandamcs@gmail.com>
2023-05-08 14:51:50 +0200

    use model-service to get sentiment

--- src/routes/restaurants/[id]/+page.svelte
+++ src/routes/restaurants/[id]/+page.svelte
@@ -4,6 +4,7 @@
   import { page } from '$app/stores';
   import Page from '../../../lib/structure/Page.svelte';
   import StarRangeInput from '../../../lib/StarRangeInput.svelte';
+  import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
   let restaurant: Restaurant;
   $: {
@@ -18,11 +19,11 @@
   let review = '';
   let starRating = 4;
 
-  let sentiment: 'positive' | 'negative' = 'positive';
+  let sentiment: 'positive' | 'negative' | undefined = undefined;
   let sentimentImg = '';
 
   $: {
-    sentiment = starRating >= 4 ? 'positive' : 'negative';
+    console.log('updating code with sentiment', sentiment);
     if (restaurant) {
       restaurantRating =
         restaurant.reviews.reduce((acc, review) => acc + review.rating, 0) /
@@ -32,6 +33,71 @@
           ? restaurant.positiveSentimentImage
           : restaurant.negativeSentimentImage;
     }
+  }
+
+  async function updateSentiment() {
+    try {
+      sentiment = await getSentiment(review);
+    } catch (err) {
+      console.error('Error getting sentiment', err);
+      sentiment = undefined;
+    }
+  }
+
+  async function getSentiment(text: string): Promise<'positive' | 'negative'> {
+    const endpointUrl = new URL(`/${decodeURIComponent(text)}`, PUBLIC_BACKEND_URL);
+    const response = await fetch(endpointUrl.toString(), { method: 'GET' });
+    if (!response.ok) {
+      throw new Error(`Error getting sentiment: ${response.statusText}`);
+    }
+    const json = await response.json();
+
+    if (!Array.isArray(json) || json.length !== 1 || typeof json[0] !== 'number') {
+      throw new Error(
+        'Invalid response from sentiment endpoint. Expected array of length 1, containing a number'
+      );
+    }
+
+    const [sentiment] = json;
+
+    if (sentiment === 0) {
+      return 'negative';
+    } else if (sentiment === 1) {
+      return 'positive';
+    } else {
+      throw new Error(`Invalid sentiment value: ${sentiment}`);
+    }
+  }
+
+  // https://thefrontendteam.com/pills/svelte-input-keyup-stop-writing-custom-event/
+  function debounce(inputFunction, timeToWaitBeforeFiringInMs = 500) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        inputFunction.apply(this, args);
+      }, timeToWaitBeforeFiringInMs);
+    };
+  }
+
+  function stopTyping(node) {
+    const handleKeyup = debounce((event: KeyboardEvent) => {
+      // (1) the debounce logic
+      if (node.contains(event.target)) {
+        // (2) restrict the event to the only referring node
+        node.dispatchEvent(new CustomEvent('stopTyping')); // (3) fire the event
+      }
+    }, 500);
+
+    // (4) add a generic keyup event
+    document.addEventListener('keyup', handleKeyup, true);
+
+    return {
+      destroy() {
+        // (5) cleanup on destroy
+        document.removeEventListener('keyup', handleKeyup, true);
+      }
+    };
   }
 
   function handleSubmit() {
@@ -47,44 +113,50 @@
 </script>
 
 {#if restaurant}
-<Page title={restaurant.name} description={restaurant.description}>
-  <img src={restaurant.image} alt="{restaurant.name} image" slot="pre-intro" />
-  <div class="rating">
-    <StarRangeInput disabled id="rating" min={1} max={5} step={1} value={restaurantRating} />
-    <span>{restaurantRating.toFixed(1)} / 5.0</span>
-  </div>
+  <Page title={restaurant.name} description={restaurant.description}>
+    <img src={restaurant.image} alt="{restaurant.name} image" slot="pre-intro" />
+    <div class="rating">
+      <StarRangeInput disabled id="rating" min={1} max={5} step={1} value={restaurantRating} />
+      <span>{restaurantRating.toFixed(1)} / 5.0</span>
+    </div>
 
-  <h2>Reviews</h2>
-  <div class="reviews">
-    {#each restaurant.reviews as review}
-      <div class="review">
-        <div class="info">
-          <h3>{review.name}</h3>
-          <p>{review.comment}</p>
+    <h2>Reviews</h2>
+    <div class="reviews">
+      {#each restaurant.reviews as review}
+        <div class="review">
+          <div class="info">
+            <h3>{review.name}</h3>
+            <p>{review.comment}</p>
+          </div>
+          <StarRangeInput disabled id="rating" min={1} max={5} step={1} value={review.rating} />
         </div>
-        <StarRangeInput disabled id="rating" min={1} max={5} step={1} value={review.rating} />
-      </div>
-    {/each}
-  </div>
-  <h2>Write a review</h2>
-  <form on:submit|preventDefault={handleSubmit}>
-    <label for="name">Name</label>
-    <input type="text" id="name" name="name" bind:value={name} />
-    <label for="rating">Stars</label>
-    <StarRangeInput id="rating" min={1} max={5} step={1} bind:value={starRating} />
-    <label for="review">Review</label>
-    <textarea id="review" name="review" bind:value={review} />
-    <p class="sentiment">Sentiment</p>
-    <span>
-      {#if sentiment}
-        <img class="sentimentImg" src={sentimentImg} alt="{sentiment} sentiment" />
-      {:else}
-        unknown
-      {/if}
-    </span>
-    <button type="submit">Submit</button>
-  </form>
-</Page>
+      {/each}
+    </div>
+    <h2>Write a review</h2>
+    <form on:submit|preventDefault={handleSubmit}>
+      <label for="name">Name</label>
+      <input type="text" id="name" name="name" bind:value={name} />
+      <label for="rating">Stars</label>
+      <StarRangeInput id="rating" min={1} max={5} step={1} bind:value={starRating} />
+      <label for="review">Review</label>
+      <textarea
+        id="review"
+        name="review"
+        bind:value={review}
+        use:stopTyping
+        on:stopTyping={updateSentiment}
+      />
+      <p class="sentiment">Sentiment</p>
+      <span>
+        {#if sentiment}
+          <img class="sentimentImg" src={sentimentImg} alt="{sentiment} sentiment" />
+        {:else}
+          unknown
+        {/if}
+      </span>
+      <button type="submit">Submit</button>
+    </form>
+  </Page>
 {:else}
   <Page title="Restaurant not found" />
 {/if}
