e24538ec1cc7855d925ae44d670b3a7120b4e7d9
Tim van Dam <timvandamcs@gmail.com>
2023-06-01 18:40:05 +0200

    use new api structure

--- src/routes/api/sentiment/+server.ts
+++ src/routes/api/sentiment/+server.ts
@@ -14,7 +14,7 @@ export const GET: RequestHandler = async (event) => {
     throw error(400, 'You must provide a text to perform sentiment analysis on');
   }
 
-  const endpointUrl = new URL(`/${decodeURIComponent(reviewText)}`, env.BACKEND_URL);
+  const endpointUrl = new URL(`/${encodeURIComponent(reviewText)}`, env.BACKEND_URL);
   const response = await fetch(endpointUrl.toString(), { method: 'GET' });
 
   if (!response.ok) {
@@ -24,16 +24,28 @@ export const GET: RequestHandler = async (event) => {
 
   const jsonResponse = await response.json();
 
-  if (
-    !Array.isArray(jsonResponse) ||
-    jsonResponse.length < 1 ||
-    typeof jsonResponse[0] !== 'number'
-  ) {
-    console.error(`Model service returned unexpected format`);
+  if (typeof jsonResponse !== 'object' || jsonResponse === null) {
+    console.error(`Model service returned unexpected format. Not an object`);
     throw error(500);
   }
 
-  const [sentiment] = jsonResponse;
+  if (!('sentiment' in jsonResponse)) {
+    console.error(`Model service returned unexpected format. Missing sentiment property`);
+    throw error(500);
+  }
+
+  if (typeof jsonResponse.sentiment !== 'number') {
+    console.error(`Model service returned unexpected format. Sentiment property is not a number`);
+    throw error(500);
+  }
+
+  const { sentiment } = jsonResponse;
+
+  if (sentiment !== 0 && sentiment !== 1) {
+    console.error(`Model service returned unexpected sentiment number '${sentiment}'`);
+    throw error(500);
+  }
+
   const sentimentText = ['negative', 'positive'][sentiment];
 
   if (!sentimentText) {
